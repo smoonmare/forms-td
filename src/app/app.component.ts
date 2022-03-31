36ba227448476e8edb9adc92f30d2b940e885dca
smoonmare <smoonmare@gmail.com>
2022-03-30 23:05:05 -0700

    Submitting form via ngSubmit and NgForm.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  ngOnInit(): void {
+      
+  }
+  
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 }
