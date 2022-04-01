800df2798b7221d6ae8f6c26417d473365c4bcaa
smoonmare <smoonmare@gmail.com>
2022-03-31 21:39:40 -0700

    Created default value with ngModel. Added textarea to test two-way-binding. Grouped controls via ngModelGroup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   @ViewChild('formData') signupForm?: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
   
   ngOnInit(): void {
       
