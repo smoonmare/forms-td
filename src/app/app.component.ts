02b636304ea1752537d83c95578970da6b276717
smoonmare <smoonmare@gmail.com>
2022-04-02 09:18:08 -0700

    Created user to store the form data. Used values to display user input.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent implements OnInit {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    secretAnswer: '',
+    gender: ''
+  }
+  submitted = false;
   
   ngOnInit(): void {
       
@@ -40,5 +48,11 @@ export class AppComponent implements OnInit {
 
   onSubmit() {
     console.log(this.signupForm);
+    this.user.username = this.signupForm?.value.userData.username;
+    this.user.email = this.signupForm?.value.userData.email;
+    this.user.secretQuestion = this.signupForm?.value.secret;
+    this.user.secretAnswer = this.signupForm?.value.questionAnswer;
+    this.user.gender = this.signupForm?.value.gender;
+    this.submitted = true;
   }
 }
